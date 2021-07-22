b43a902957d0a74b2fdee25a6155f50ca54ae52d
Theophil Dio <43618312+theophildio@users.noreply.github.com>
2021-07-22 11:48:58 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+	});
+
+
+	// Venobox 
+	$(document).ready(function(){
+    $('.venobox').venobox({
+			spinner 			: 'wandering-cubes',
+			framewidth 		: '800px',
+			frameheight 	: '500px',
+			border				: '10px',
+			share 				: ['facebook', 'twitter', 'linkedin', 'pinterest', 'download'],
+			arrowsColor		: '#B6B6B6'
+		}); 
+	});
+	
+	
+})
+
+
